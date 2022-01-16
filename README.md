c5c4b456bf02ee235c79ac335d11b97ef0f03ba2
Travis-Lee <lixiang.fr@hotmail.com>
2022-01-16 13:34:34 +0800

    first commit for start learn vulkan API

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Etude_Vulkan
+
+#This project  for System of MacOS
+
+#first need to install vulkan sdk from: https://vulkan.lunarg.com/sdk/home on system MacOS 
+
+#and also, we need to install glfw from: https://github.com/glfw/glfw. it's can be directly download last version 
---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+int main()
+{
+
+    glfwInit();
+ 
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+ 
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+ 
+	glfwDestroyWindow(window);
+	
+    glfwTerminate();
+	
+    return 0;
+
+}
