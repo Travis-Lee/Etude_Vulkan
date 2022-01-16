87c5ba2b8051797f1707cc7bd5d82fca5a0632ff
Travis-Lee <lixiang.fr@hotmail.com>
2022-01-16 16:04:52 +0800

    add new example for learning vulkan api on system of macOS

--- bash/run_example.sh
+++ bash/run_example.sh
@@ -1,1 +1,3 @@
 ./build_macOS_x86/macOS-release/StartVulkan
+./build_macOS_x86/macOS-release/ex01
+./build_macOS_x86/macOS-release/ex02
---
+++ example_01/main.cpp
@@ -0,0 +1,35 @@
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication {
+public:
+    void run() {
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+private:
+    void initVulkan() {
+      printf("init vulkan engine!\n");
+    }
+    void mainLoop() {
+     printf("start work with vulkan!\n");
+    }
+    void cleanup() {
+     printf("clean buffer for vullan\n");
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
